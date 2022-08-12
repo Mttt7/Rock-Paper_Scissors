2418a33fdcfb8006f28d41df07bde5634111f051
mttt <groobytt@gmail.com>
2022-08-12 13:52:40 +0200

    add hide and show game/menu feature

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-getComputerChoice(){
-    
-}
+/* HIDING  AND SHOWING MENU*/
+const menu = document.getElementById("menu")
+const startBtn = document.getElementById("start-button")
+const exitBtn = document.getElementById("exit-button")
+exitBtn.style.display = 'none'
+
+startBtn.addEventListener('click',function start(){
+    console.log("x")
+    menu.style.display = 'none'
+    exitBtn.style.display = 'block'
+
+
+})
+
+exitBtn.addEventListener('click',function start(){
+    console.log("x")
+    menu.style.display = 'block'
+    exitBtn.style.display = 'none'
+
+
+})
